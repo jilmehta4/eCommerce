3c993392814d8e10a14820da2fdf819a2e72966b
Jil <jilmehta52@gmail.com>
2025-07-27 18:57:07 -0400

    First Commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const productRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
+
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
